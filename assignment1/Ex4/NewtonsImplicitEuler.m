9253603746b59ab65593175d35407fa2b4ce44c2
Miguel <miguel.suau@gmail.com>
2017-03-07 11:40:29 +0100

    Implicit Euler AdaptiveStep modified, Newtons Implicitdebugged

---
+++ assignment1/Ex4/NewtonsImplicitEuler.m
@@ -0,0 +1,19 @@
+function y = NewtonsImplicitEuler(FunJac,tk,yk,h,y0,tol,maxit,varargin)
+
+k = 0;
+t = tk + h;
+y = y0;
+[f,J] = feval(FunJac,t,y,varargin{:});
+R = y -h*f - yk;
+I = eye(size(yk,1));
+
+while ((k < maxit) && (norm(R,'inf') > tol))
+    k = k+1;
+    dRdy = I - h*J;
+    dy = dRdy\R;
+    y = y - dy;
+    [f,J] = feval(FunJac,t,y,varargin{:});
+    R = y - h*f - yk;
+end
+
+end
