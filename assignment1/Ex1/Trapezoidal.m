6a6a9101b6cabba9a2af6095276bb14b78992988
Miguel <miguel.suau@gmail.com>
2017-03-11 11:01:24 +0100

    DOPRI54

--- assignment1/Ex1/Trapezoidal.m
+++ assignment1/Ex1/Trapezoidal.m
@@ -1,16 +1,21 @@
 function [T,Y] = Trapezoidal(funJac,tspan,n,y0,varargin)
 
 % Compute step size
-h = (tspan(2)-tspan(1))/n;
+h = (tspan(2)-tspan(1))/(n-1);
 
 % Set tolerance and max number of iterations for the solver
 tol = 1.0e-8;
 maxit = 100;
 
+% Allocate memory
+m = size(y0,1);
+T = zeros(n,1);
+Y = zeros(m,n);
+
 % Trapezoidal method
 Y(:,1) = y0;
 T(1) = tspan(1);
-for i=1:n
+for i=1:(n-1)
     
     T(i+1) = T(i)+h;
  
--- assignment1/Ex2/exercise2.m
+++ assignment1/Ex2/exercise2.m
@@ -6,9 +6,6 @@ n = 10000;
 y0 = [2; 0];
 mu = 10;
 
-abstol = 10e-6;
-reltol = 10e-8;
-
 %% Explicit Euler
 %[T,Y] = ExplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
 
@@ -18,6 +15,24 @@ reltol = 10e-8;
 %% Trapezoidal
 [T3,Y3] = Trapezoidal(@VanderPolfunjac,tspan,n,y0,mu);
 
-plot(T1,Y1(:,1))
+%% Classical Runge-Kutta
+[T4,Y4] = ClassicalRungeKutta(@VanderPolfunjac,tspan,n,y0,mu);
+
+plot(T3,Y3(:,2))
 hold on
-plot(T3,Y3(:,1))
+plot(T4,Y4(:,2))
+
+%% DOPRI54
+% Call ERKSolverErrorEstimationParameters.m to get Butcher's Tableau
+
+butcher = ERKSolverErrorEstimationParameters('DOPRI54');
+[T5,Y5,Err5] = ExplicitRungeKutta(@VanderPolfunjac,tspan,y0,n,butcher,mu);
+
+% DIFFERENCE BETWEEN THE TWO DOPRI54
+butcher.b = [5179/57600; 0; 7571/16695; 393/640; -92097/339200; 187/2100; 1/40];
+[T6,Y6,Err6] = ExplicitRungeKutta(@VanderPolfunjac,tspan,y0,n,butcher,mu);
+
+% Explicit methods won't work for mu large. That is, for very stiff
+% problems GIVE REASON AND EXPLAIN WHAT STIFF MEANS.
+
+% Look up embeded methods error estimation
