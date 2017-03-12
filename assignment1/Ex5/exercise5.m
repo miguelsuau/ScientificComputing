5969c463fa3b63a358f52fb2033b16c29f570e1f
Michal Baumgartner <miso.baumgartner@gmail.com>
2017-03-12 16:56:23 +0100

    Exercise 5 added more plots

--- assignment1/Ex5/exercise5.m
+++ assignment1/Ex5/exercise5.m
@@ -3,7 +3,6 @@ addpath('../')
 t0 = 0;
 tf = 50;
 x0 = [2; 0];
-n = 10000;
 mu = 3; % and then 100
 h0 = 0.01;
 absTol = 1e-6;
@@ -17,17 +16,38 @@ x = Xout;
 
 % plot results
 figure
-subplot(2,2,1)
-plot(t,x(:,1),'.-b'), title('Van der Pol solution')
-subplot(2,2,2)
-plot(t,x(:,2),'.-r'), xlabel('Time')
+subplot(4,2,1)
+plot(t,x(:,1),'.-b'), title('Van der Pol solution - ESDIRK_{23}^{Adaptive} \mu = 3'), xlim([t0 tf])
+subplot(4,2,2)
+plot(t,x(:,2),'.-r'), xlabel('Time'), xlim([t0 tf])
 
 % ESDIRK23
 [Tout,Xout,info,stats] = ESDIRK23(@VanderPolFun,@VanderPolJac,t0,tf,x0,h0,absTol,relTol,mu);
 t2 = Tout;
 x2 = Xout;
 
-subplot(2,2,3)
-plot(t2,x2(:,1),'.-b'), title('Van der Pol solution')
-subplot(2,2,4)
-plot(t2,x2(:,2),'.-r'), xlabel('Time')
+subplot(4,2,3)
+plot(t2,x2(:,1),'.-b'), title('Van der Pol solution - ESDIRK_{23}^{Fixed} \mu = 3'), xlim([t0 tf])
+subplot(4,2,4)
+plot(t2,x2(:,2),'.-r'), xlabel('Time'), xlim([t0 tf])
+
+% change mu to 100
+mu = 100;
+h0 = 0.01;
+tf = 1000;
+
+[Tout,Xout,info,stats] = ESDIRK23_Adaptive(@VanderPolFun,@VanderPolJac,t0,tf,x0,h0,absTol,relTol,mu);
+t = Tout;
+x = Xout;
+subplot(4,2,5)
+plot(t,x(:,1),'.-b'), title('Van der Pol solution - ESDIRK_{23}^{Adaptive} \mu = 100'), xlim([t0 tf])
+subplot(4,2,6)
+plot(t,x(:,2),'.-r'), xlabel('Time'), xlim([t0 tf])
+
+[Tout,Xout,info,stats] = ESDIRK23(@VanderPolFun,@VanderPolJac,t0,tf,x0,h0,absTol,relTol,mu);
+t2 = Tout;
+x2 = Xout;
+subplot(4,2,7)
+plot(t2,x2(:,1),'.-b'), title('Van der Pol solution - ESDIRK_{23}^{Fixed} \mu = 100'), xlim([t0 tf])
+subplot(4,2,8)
+plot(t2,x2(:,2),'.-r'), xlabel('Time'), xlim([t0 tf])
