a1baef5778c8687244c79f9a8b208b0ed38412b6
Miguel <miguel.suau@gmail.com>
2017-03-16 08:57:57 +0100

    Before merging

--- assignment1/Ex1/ex1_TestEquation.m
+++ assignment1/Ex1/ex1_TestEquation.m
@@ -3,7 +3,7 @@
 %% TEST EQUATION 
 
 tspan = [0; 10];
-n = 50;
+n = 30;
 x0 = 1;
 lambda = -1;
 
@@ -20,16 +20,42 @@ X = exp(lambda*T);
 %% Trapezoidal
 [T3,X3] = Trapezoidal(@TestEquation,tspan,n,x0,lambda);
 
+%% HARMONIC
+x0 = [1; 0];
+% Analyticial solution
+TH = linspace(tspan(1),tspan(2),n)';
+XH = cos(T);
+
+% Explicit Euler
+[T1H,X1H] = ExplicitEuler(@HarmonicOscillator,tspan,n,x0);
+
+% Implicit Euler
+[T2H,X2H] = ImplicitEuler(@HarmonicOscillator,tspan,n,x0);
+subplot(1,2,1)
+% Trapezoidal
+[T3H,X3H] = Trapezoidal(@HarmonicOscillator,tspan,n,x0);
+
+
+x0 = 1;
 %% Classical Runge-Kutta
 [T4,X4] = ClassicalRungeKutta(@TestEquation,tspan,n,x0,lambda);
-
-plot(T,X(:),'--k')
+subplot(2,1,1)
+plot(T,X(:),'--k','LineWidth',1.5)
 hold on
 plot(T1,X1(:),'LineWidth',1.5)
 plot(T2,X2(:),'LineWidth',1.5)
-
-legend('Analytical solution','Explicit Euler','Implicit Euler')
-
+plot(T3,X3(:),'LineWidth',1.5)
+leg1 = legend('Analytical solution','Explicit Euler','Implicit Euler','Trapezoidal');
+set(leg1,'fontsize',8)
+subplot(2,1,2)
+plot(TH,XH(:),'--k','LineWidth',1.5)
+hold on
+plot(T1H,X1H(:,1),'LineWidth',1.5)
+plot(T2H,X2H(:,1),'LineWidth',1.5)
+plot(T3H,X3H(:,1),'LineWidth',1.5)
+leg2 = legend('Analytical solution','Explicit Euler','Implicit Euler','Trapezoidal');
+set(leg2,'fontsize',8)
+print('solution1','-dpng')
 %% DOPRI54
 % Call ERKSolverErrorEstimationParameters.m to get Butcher's Tableau
 
@@ -49,7 +75,7 @@ Gerr = zeros(6,10);
 
 for i=1:10
     n = ceil((tspan(2) - tspan(1))/h(i) + 1);
-    n2 = ceil((tspan(2) - tspan(1))/h(i)*2 + 1);
+    n2 = ceil((tspan(2) - tspan(1))/h(i) + 1)*2;
     % Analytical Solution
     T = linspace(tspan(1),tspan(2),n);
     X = exp(lambda*T);
@@ -60,7 +86,7 @@ for i=1:10
     [T1Double,X1Double] = ExplicitEuler(@TestEquation,tspan,n2,x0,lambda);
     
     Lerr(1,i) = abs(X(2) - X1(2));
-    LerrHat(1,i) = abs(X1Double(2) - X1(2));
+    LerrHat(1,i) = 2*abs(X1Double(3) - X1(2));
     Gerr(1,i) = abs(X(T == 10) - X1(abs(T1 - 10) <= 1e-3));
     
     % Implicit Euler
@@ -68,7 +94,7 @@ for i=1:10
     [T2Double,X2Double] = ImplicitEuler(@TestEquation,tspan,n2,x0,lambda);
     
     Lerr(2,i) = abs(X(2) - X2(2));
-    LerrHat(2,i) = abs(X2Double(2) - X2(2));
+    LerrHat(2,i) = 2*abs(X2Double(3) - X2(2));
     Gerr(2,i) = abs(X(T == 10) - X2(abs(T2 - 10) <= 1e-3));
     
     % Trapezoidal
@@ -76,7 +102,7 @@ for i=1:10
     [T3Double,X3Double] = Trapezoidal(@TestEquation,tspan,n2,x0,lambda);
     
     Lerr(3,i) = abs(X(2) - X3(2));
-    LerrHat(3,i) = abs(X3Double(2) - X3(2));
+    LerrHat(3,i) = 4/3*abs(X3Double(3) - X3(2));
     Gerr(3,i) = abs(X(T == 10) - X3(abs(T3 - 10) <= 1e-3));
     
     % Classical Runge-Kutta
@@ -85,7 +111,7 @@ for i=1:10
                           @TestEquation,tspan,n2,x0,lambda);
     
     Lerr(4,i) = abs(X(2) - X4(2));
-    LerrHat(4,i) = abs(X4Double(2) - X4(2));
+    LerrHat(4,i) = 16/15*abs(X4Double(3) - X4(2));
     Gerr(4,i) = abs(X(T == 10) - X4(abs(T4 - 10) <= 1e-3));
     
     % DOPRI54
@@ -113,26 +139,53 @@ end
 figure
 
 % local error
-loglog(h,Lerr(1:3,:))
-legend('Explicit Euler','Implicit Euler','Trapezoidal','location','southeast')
-figure
-loglog(h,Lerr(4:6,:))
-legend('Classic Runge Kutta','DOPRI54','DOPRI542','location','southeast')
+subplot(1,2,1)
+loglog(h,Lerr(1:3,:),'LineWidth',1.5)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 2;
+loglog(h,h.^2,'--',h,h.^3,'--')
+r = legend('Explicit Euler','Implicit Euler','Trapezoidal','$O(h^2)$','$O(h^3)$','location','southeast');
+set(r,'Interpreter','latex')
+subplot(1,2,2)
+loglog(h,Lerr(4:6,:),'LineWidth',1.5)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 1;
+loglog(h,h.^5,'--',h,h.^6,'--')
+%axis([10e-3 10e-2 10e-16 10e-4 ])
+r = legend('Runge Kutta 4','DOPRI54 (5)','DOPRI54 (4)','$O(h^5)$','$O(h^6)$','location','northwest');
+set(r,'Interpreter','latex')
+
+print('LocalErrorTest1','-dpng')
 
 % Global error t = 10
 figure
-loglog(h,Gerr(1:3,:))
-legend('Explicit Euler','Implicit Euler','Trapezoidal','location','southeast')
-title('Global error')
-figure
-loglog(h,Gerr(4:6,:))
-legend('Classic Runge Kutta','DOPRI54','DOPRI542','location','southeast')
-title('Global error')
+loglog(h,Gerr(1:3,:),'LineWidth',1.5)
+hold on
+loglog(h,Gerr(4:6,:),'LineWidth',1.5)
+legend('Explicit Euler','Implicit Euler','Trapezoidal','Classic Runge Kutta','DOPRI54 (5)','DOPRI54 (4)','location','southeast')
 
+print('GlobalErrorTest','-dpng')
 % local error estimates
 figure
-loglog(h,LerrHat(1:3,:))
-legend('Explicit Euler','Implicit Euler','Trapezoidal','location','southeast')
-figure
-loglog(h,LerrHat(4:6,:))
-legend('Classic Runge Kutta','DOPRI54','DOPRI542','location','southeast')
+subplot(1,2,1)
+loglog(h,LerrHat(1:3,:),'LineWidth',1.5)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 1;
+loglog(h,Lerr(1:3,:),'--')
+r = legend('Explicit Euler','Implicit Euler','Trapezoidal','True Explicit', ...
+       'True Implicit','True Trapezoidal','location','southeast')
+set(r,'fontsize',7)
+subplot(1,2,2)
+loglog(h,LerrHat(4:6,:),'LineWidth',1.5)
+hold on
+ax = gca;
+ax.ColorOrderIndex = 1;
+loglog(h,Lerr(4:6,:),'--')
+r = legend('Runge Kutta 4','DOPRI54 (5)','DOPRI54 (4)', ...
+       'Runge Kutta true','DOPRI54 (5) true', ...
+       'DOPRI54 (4) true','location','southeast')
+set(r,'fontsize',7)
+print('LocalEstimateTest1','-dpng')
--- assignment1/Ex2/exercise2.m
+++ assignment1/Ex2/exercise2.m
@@ -2,15 +2,16 @@
 addpath('../')
 addpath('../Ex1')
 tspan = [0; 50];
-n = 10000;
 y0 = [2; 0];
-mu = 10;
+n = 1000;
+h = (tspan(2)-tspan(1))/(n-1);
+mu = 3;
 
 %% Explicit Euler
-%[T,Y] = ExplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+[T1,Y1] = ExplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
 
 %% Implicit Euler
-[T1,Y1] = ImplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+[T2,Y2] = ImplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
 
 %% Trapezoidal
 [T3,Y3] = Trapezoidal(@VanderPolfunjac,tspan,n,y0,mu);
@@ -18,9 +19,6 @@ mu = 10;
 %% Classical Runge-Kutta
 [T4,Y4] = ClassicalRungeKutta(@VanderPolfunjac,tspan,n,y0,mu);
 
-plot(T3,Y3(:,2))
-hold on
-plot(T4,Y4(:,2))
 
 %% DOPRI54
 % Call ERKSolverErrorEstimationParameters.m to get Butcher's Tableau
@@ -28,11 +26,121 @@ plot(T4,Y4(:,2))
 butcher = ERKSolverErrorEstimationParameters('DOPRI54');
 [T5,Y5,Err5] = ExplicitRungeKutta(@VanderPolfunjac,tspan,y0,n,butcher,mu);
 
-% DIFFERENCE BETWEEN THE TWO DOPRI54
-butcher.b = [5179/57600; 0; 7571/16695; 393/640; -92097/339200; 187/2100; 1/40];
-[T6,Y6,Err6] = ExplicitRungeKutta(@VanderPolfunjac,tspan,y0,n,butcher,mu);
 
-% Explicit methods won't work for mu large. That is, for very stiff
-% problems GIVE REASON AND EXPLAIN WHAT STIFF MEANS.
+subplot(2,1,1)
+l1 = plot(T1,Y1(:,1));
+hold on
+l2 = plot(T2,Y2(:,1));
+l3 = plot(T3,Y3(:,1));
+l4 = plot(T4,Y4(:,1));
+l5 = plot(T5,Y5(:,1));
+axis([0 50 -Inf Inf])
+subplot(2,1,2)
+l6 = plot(T1,Y1(:,2));
+hold on
+l7 = plot(T2,Y2(:,2));
+l8 = plot(T3,Y3(:,2));
+l9 = plot(T4,Y4(:,2));
+l10 = plot(T5,Y5(:,2));
+axis([0 50 -Inf Inf])
+hL = legend([l1,l2,l3,l4,l5],{'Explicit Euler','Implicit Euler','Trapezoidal','Classic Runge Kutta','DOPRI54 (5)'});
 
-% Look up embeded methods error estimation
+%% MU = 100
+
+tspan = [0; 250];
+y0 = [2; 0];
+n = 100000;
+h = (tspan(2)-tspan(1))/(n-1);
+
+mu = 100;
+figure
+%% Explicit Euler
+[T1,Y1] = ExplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+
+%% Implicit Euler
+[T2,Y2] = ImplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+
+%% Trapezoidal
+[T3,Y3] = Trapezoidal(@VanderPolfunjac,tspan,n,y0,mu);
+
+%% Classical Runge-Kutta
+[T4,Y4] = ClassicalRungeKutta(@VanderPolfunjac,tspan,n,y0,mu);
+
+%% DOPRI54
+% Call ERKSolverErrorEstimationParameters.m to get Butcher's Tableau
+
+butcher = ERKSolverErrorEstimationParameters('DOPRI54');
+[T5,Y5,Err5] = ExplicitRungeKutta(@VanderPolfunjac,tspan,y0,n,butcher,mu);
+
+subplot(2,1,1)
+l1 = plot(T1,Y1(:,1));
+hold on
+l2 = plot(T2,Y2(:,1));
+l3 = plot(T3,Y3(:,1));
+l4 = plot(T4,Y4(:,1));
+l5 = plot(T5,Y5(:,1));
+axis([0 250 -Inf Inf])
+subplot(2,1,2)
+l6 = plot(T1,Y1(:,2));
+hold on
+l7 = plot(T2,Y2(:,2));
+l8 = plot(T3,Y3(:,2));
+l9 = plot(T4,Y4(:,2));
+l10 = plot(T5,Y5(:,2));
+axis([0 250 -Inf Inf])
+hL = legend([l1,l2,l3,l4,l5],{'Explicit Euler','Implicit Euler','Trapezoidal','Classic Runge Kutta','DOPRI54 (5)'});
+
+%% ERROR ESTIMATION
+
+h = logspace(-1,-3,10);
+Lerr = zeros(5,10);
+tspan = [0; 250];
+y0 = [2; 0];
+mu = 100;
+for i=1:10
+    
+    n = ceil((tspan(2) - tspan(1))/h(i) + 1);
+    n2 = ceil((tspan(2) - tspan(1))/h(i) + 1)*2;
+
+    % Explicit Euler
+    [T1,X1] = ExplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+    
+    [T1Double,X1Double] = ExplicitEuler(@VanderPolfunjac,tspan,n2,y0,mu);
+    
+    Lerr(1,i) = 2*abs(X1Double(3,1) - X1(2,1));
+    
+    % Implicit Euler
+    [T2,X2] = ImplicitEuler(@VanderPolfunjac,tspan,n,y0,mu);
+    [T2Double,X2Double] = ImplicitEuler(@VanderPolfunjac,tspan,n2,y0,mu);
+    
+    Lerr(2,i) = 2*abs(X2Double(3,1) - X2(2,1));
+    
+    % Trapezoidal
+    [T3,X3] = Trapezoidal(@VanderPolfunjac,tspan,n,y0,mu);
+    [T3Double,X3Double] = Trapezoidal(@VanderPolfunjac,tspan,n2,y0,mu);
+    
+    Lerr(3,i) = 4/3*abs(X3Double(3,1) - X3(2,1));
+    
+    % Classical Runge-Kutta
+    [T4,X4] = ClassicalRungeKutta(@VanderPolfunjac,tspan,n,y0,mu);
+    [T4Double,X4Double] = ClassicalRungeKutta( ...
+                          @VanderPolfunjac,tspan,n2,y0,mu);
+    
+    Lerr(4,i) = 16/15*abs(X4Double(3,1) - X4(2,1));
+    
+    % DOPRI54
+    % Call ERKSolverErrorEstimationParameters.m to get Butcher's Tableau
+
+    butcher = ERKSolverErrorEstimationParameters('DOPRI54');
+    [T5,X5,Err5] = ExplicitRungeKutta( ...
+                   @VanderPolfunjac,tspan,y0,n,butcher,mu);
+               
+    Lerr(5,i) = abs(Err5(2,1));
+    
+end
+
+% local error estimates
+figure
+loglog(h,Lerr,'LineWidth',1.5)
+r = legend('Explicit Euler','Implicit Euler','Trapezoidal','Runge Kutta 4','DOPRI54 (5)')
+print('VanderPolError','-dpng')
