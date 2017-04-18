ce96106b5989fb99c424604a11e69ced5c0e4775
Miguel <miguel.suau@gmail.com>
2017-04-18 18:59:58 +0200

    Ex3 modified

--- assignment2/Ex3/Ex3.m
+++ assignment2/Ex3/Ex3.m
@@ -1,17 +1,16 @@
 
 
 %% 3.2 Under/Over-relaxed Jacobi Smoothing
-feig = @(p,q,h,omega) (1-omega) + omega*2/h^2*(cos(p*pi*h)-1+cos(q*pi*h)-1);
-m = 100;
+feig = @(p,q,h,omega) (1-omega)+omega.*2/h^2*(cos(p.*pi*h)-1+cos(q.*pi*h)-1);
+m = 10000;
 omega = linspace(0,2,100);
 h = 1/(m+1);
-eig = zeros(m/2-m,m/2-m);
+p = m/2:m;
+q = m/2:m;
+[P,Q] = meshgrid(p,q);
 maxeig = zeros(100,1);
+
 for i = 1:100
-    for p = m/2:m
-        for q = m/2:m
-            eig(p,q) = feig(p,q,h,omega(i));
-        end
-    end
+    eig = feig(P,Q,h,omega(i));
     maxeig(i) = max(max(abs(eig)));
 end
