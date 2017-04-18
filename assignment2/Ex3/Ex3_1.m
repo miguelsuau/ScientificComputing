f14ecca1c68926cd19d5265a27f58fb88e80a14d
Michal Baumgartner <miso.baumgartner@gmail.com>
2017-04-18 11:21:38 +0200

    Ex 3.1 pcg fixes

--- assignment2/Ex3/Ex3_1.m
+++ assignment2/Ex3/Ex3_1.m
@@ -1,6 +1,8 @@
 m = 30;
 h = 1/(m+1);
 [X,Y] = meshgrid(0:h:1,0:h:1);
+Xint = X(2:m+1,2:m+1);
+Yint = Y(2:m+1,2:m+1);
 
 % Change to -Au=-F (`pcg` requires positive definite system)
 f0 = @(X,Y) -16 * (pi ^ 2) * (...
@@ -8,11 +10,11 @@ f0 = @(X,Y) -16 * (pi ^ 2) * (...
     + cos((4 * pi * X .* Y)) .* (Y .^ 2)...
     + 2 * sin((4 * pi * (X + Y)))...
 );
-b = -f0(X,Y);
-Afun = @(u) reshape(-Amult(u,m+2), (m+2)*(m+2), 1);
+b = -f0(Xint,Yint);
+Afun = @(u) reshape(-Amult(u,m), (m)^2, 1);
 
-u = pcg(Afun,b(:),1e-6,10);
+u = pcg(Afun,b(:));
 % max(max(abs(Amult(u,m+2) - reshape(poisson5(m+2)*u,m+2,m+2))))
 
 figure(1)
-surf(X,Y,reshape(u,m+2,m+2));
+surf(Xint,Yint,reshape(u,m,m));
