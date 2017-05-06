b9f3c61ba9a73c6799a654ba004db819c4ad1ead
Miguel <miguel.suau@gmail.com>
2017-05-06 15:32:46 +0200

    latex

--- assignment3/Ex3/Ex3.m
+++ assignment3/Ex3/Ex3.m
@@ -3,12 +3,18 @@ h = 0.1;
 k = h^2/6;
 epsilon = 0.5;
 tmax = 1;
-U = BurgerSolver(@boundaryFun,h,k,epsilon,tmax);
-mesh(U);
+U1 = BurgerSolver(@boundaryFun,h,k,epsilon,tmax);
+mesh(U1);
 figure
 %% true solution
 x = linspace(-1,1,100);
 t = linspace(0,1,100);
-[X,T] = meshgrid(x,t);
-U = boundaryFun(X,T,epsilon);
-mesh(X,T,U)
+[X,T] = ndgrid(x,t);
+U2 = boundaryFun(X,T,epsilon);
+mesh(X,T,U2)
+%% 3.3
+h = 0.1;
+k = h^2;
+tmax = 1.6037/pi;
+epsilon = 0.01/pi;
+U3 = BurgerSolver2(h,k,epsilon,tmax);
